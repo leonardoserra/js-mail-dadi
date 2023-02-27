5f15a5acf74fe8dd4a676dabd75dd56317554751
leonardoserra <serra.leonardo92@gmail.com>
2023-02-27 17:37:05 +0100

    semplificato il codice js

--- mail/js/main.js
+++ mail/js/main.js
@@ -7,24 +7,35 @@
 
 const mailList = ['mario@gmail.com', 'luca@gmail.com', 'anna@gmail.com', 'peppiniello@gmail.com', 'erMaggico@gmail.com'];
 const message = document.getElementById('message');
-
+let interruttore = 'accesso negato';
 console.log(mailList);
 
 let userMail = prompt('inserisci la tua mail per accedere');
 for (let i = 0; i < mailList.length; i++) {
     
     if (userMail == mailList[i]){
-        console.log('benvenuto!');
-        message.innerHTML = 'benvenuto!';
-
-        i = mailList.length - 1;
-    } else if(i == mailList.length -1){
-        console.log('accesso negato');
-        message.innerHTML = 'accesso negato';
-
-    }
+        interruttore = 'benvenuto!';
     
+    }
 }
+
+message.innerHTML = interruttore;
+
+
+// for (let i = 0; i < mailList.length; i++) {
+    
+//     if (userMail == mailList[i]){
+//         console.log('benvenuto!');
+//         message.innerHTML = 'benvenuto!';
+//         i = mailList.length - 1;
+
+//     } else if(i == mailList.length - 1){
+//         console.log('accesso negato');
+//         message.innerHTML = 'accesso negato';
+
+//     }
+    
+// }
 
 
     
