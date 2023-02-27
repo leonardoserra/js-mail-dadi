0c20ab6a074765ce7fc7a773f8b69dcaa1085bfd
leonardoserra <serra.leonardo92@gmail.com>
2023-02-27 15:30:27 +0100

    creato button che legge value di mail e da message

--- mail/bonus/js/main.js
+++ mail/bonus/js/main.js
@@ -9,17 +9,27 @@ const mailList = ['mario@gmail.com', 'luca@gmail.com', 'anna@gmail.com', 'peppin
 
 console.log(mailList);
 
-let userMail = prompt('inserisci la tua mail per accedere');
-for (let i = 0; i < mailList.length; i++) {
-    
-    if (userMail == mailList[i]){
-        console.log('benvenuto!');
-        i = mailList.length - 1;
-    } else if(i == mailList.length -1){
-        console.log('accesso negato');
+const button = document.getElementById('button');
+const userMailField = document.getElementById('mail');
+const message = document.getElementById('message');
+button.addEventListener('click', function(){
+    let userMail = userMailField.value;
+    console.log(userMail);
+
+    for (let i = 0; i < mailList.length; i++) {
+        
+        if (userMail == mailList[i]){
+            console.log('benvenuto!');
+            message.innerHTML = 'benvenuto!';
+            i = mailList.length - 1;
+        } else if(i == mailList.length -1){
+            console.log('accesso negato');
+            message.innerHTML = 'accesso negato';
+        }
+        
     }
-    
-}
+})
+
 
 
     
