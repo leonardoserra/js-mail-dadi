14ce5cf0b612e3d484dc2ae77577b0537b60999b
leonardoserra <serra.leonardo92@gmail.com>
2023-02-27 14:04:22 +0100

    creo array con list mail

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+    Mail
+    Chiedi all’utente la sua email,
+    controlla che sia nella lista di chi può accedere,
+    stampa un messaggio appropriato sull’esito del controllo.
+ */
+
+    const mailList = ['mario@gmail.com', 'luca@gmail.com', 'anna@gmail.com', 'peppiniello@gmail.com'];
+
+    console.log(mailList);
