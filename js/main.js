f8c568234317874ae8b1c10864ad0ba6d9adf226
leonardoserra <serra.leonardo92@gmail.com>
2023-02-27 14:57:08 +0100

    gioco dadi fatto con array

--- js/main.js
+++ js/main.js
@@ -5,18 +5,42 @@
     stampa un messaggio appropriato sull’esito del controllo.
  */
 
-    const mailList = ['mario@gmail.com', 'luca@gmail.com', 'anna@gmail.com', 'peppiniello@gmail.com'];
+const mailList = ['mario@gmail.com', 'luca@gmail.com', 'anna@gmail.com', 'peppiniello@gmail.com', 'erMaggico@gmail.com'];
 
-    console.log(mailList);
+console.log(mailList);
 
-    let userMail = prompt('inserisci la tua mail per accedere');
-    for (let i = 0; i < mailList.length; i++) {
-        
-        if (userMail == mailList[i]){
-            console.log('benvenuto!');
-            i = mailList.length - 1;
-        } else if(i == mailList.length -1){
-            console.log('accesso negato');
-        }
-        
+let userMail = prompt('inserisci la tua mail per accedere');
+for (let i = 0; i < mailList.length; i++) {
+    
+    if (userMail == mailList[i]){
+        console.log('benvenuto!');
+        i = mailList.length - 1;
+    } else if(i == mailList.length -1){
+        console.log('accesso negato');
     }
+    
+}
+
+/*
+    Gioco dei dadi
+    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+const points = [];
+
+
+
+for(let x = 0; x<=1; x++){
+    points.push(Math.floor(Math.random() * 6 + 1));
+    console.log(points[x]);
+}
+
+if(points[0] < points[1]){
+    console.log('vince il computer');
+} else if(points[0] > points[1]){
+    console.log('vince il player');
+} else if(points[0] = points[1]){
+    console.log('pareggio');
+}
+    
