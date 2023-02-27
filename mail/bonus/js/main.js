2347168ac041131917ab445cad21d0da5d30550b
leonardoserra <serra.leonardo92@gmail.com>
2023-02-27 15:07:43 +0100

    create cartelle bonus per creare struttura e stile

---
+++ mail/bonus/js/main.js
@@ -0,0 +1,25 @@
+/*
+    Mail
+    Chiedi all’utente la sua email,
+    controlla che sia nella lista di chi può accedere,
+    stampa un messaggio appropriato sull’esito del controllo.
+ */
+
+const mailList = ['mario@gmail.com', 'luca@gmail.com', 'anna@gmail.com', 'peppiniello@gmail.com', 'erMaggico@gmail.com'];
+
+console.log(mailList);
+
+let userMail = prompt('inserisci la tua mail per accedere');
+for (let i = 0; i < mailList.length; i++) {
+    
+    if (userMail == mailList[i]){
+        console.log('benvenuto!');
+        i = mailList.length - 1;
+    } else if(i == mailList.length -1){
+        console.log('accesso negato');
+    }
+    
+}
+
+
+    
